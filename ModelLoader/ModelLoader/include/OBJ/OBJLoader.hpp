d26be1afe226514c270355fa9562ad270ec6da5a
gon027 <gon27.tk08@gmail.com>
2025-07-14 21:26:50 +0900

    Objファイルを読み込むクラスを作成

---
+++ ModelLoader/ModelLoader/include/OBJ/OBJLoader.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "OBJData.hpp"
+
+namespace model::obj {
+
+	class ObjLoader {
+	public:
+		ObjLoader() = default;
+		~ObjLoader() = default;
+
+		bool load(const std::string& _fileName);
+
+		inline std::vector<Vertex3>& getVertex() {
+			return vertices;
+		}
+
+		inline std::vector<Vertex2>& getTexcoord() {
+			return texcoords;
+		}
+
+		inline std::vector<Vertex3>& getNormal() {
+			return normals;
+		}
+
+		inline std::vector<ObjVertex>& getObjVertex() {
+			return objVertices;
+		}
+
+	private:
+		bool loadObjFile();
+		bool loadMtlFile();
+
+	private:
+		// std::vector<ObjVertex> objVec;
+
+		std::vector<Vertex3> vertices;
+		std::vector<Vertex2> texcoords;
+		std::vector<Vertex3> normals;
+
+
+		// 最終的に出力する頂点
+		std::vector<ObjVertex> objVertices;
+
+		std::string mtlFileName;
+	};
+
+}
