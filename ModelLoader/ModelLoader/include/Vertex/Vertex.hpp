a5076eae25e71419f66cb26eec02a7b8b163d66c
gon027 <gon27.tk08@gmail.com>
2025-11-03 19:24:18 +0900

    構造体にコンストラクタを追加

--- ModelLoader/ModelLoader/include/Vertex/Vertex.hpp
+++ ModelLoader/ModelLoader/include/Vertex/Vertex.hpp
@@ -5,12 +5,22 @@ namespace model {
 	struct Vertex2 {
 		float x;
 		float y;
+
+	public:
+		Vertex2(float _x = 0.0f, float _y = 0.0f)
+			: x{ _x }, y{ _y }
+		{}
 	};
 
 	struct Vertex3 {
 		float x;
 		float y;
 		float z;
+
+	public:
+		Vertex3(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f)
+			: x{ _x }, y{ _y}, z{ _z }
+		{}
 	};
 
 	struct Vertex4 {
@@ -18,6 +28,11 @@ namespace model {
 		float y;
 		float z;
 		float w;
+
+	public:
+		Vertex4(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f, float _w = 0.0f)
+			: x{ _x }, y{ _y}, z{ _z}, w{ _w}
+		{}
 	};
 
 }
